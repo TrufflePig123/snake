ff379fda46ba7c9de8137d47a3e6302aa8b5f0c4
Gabe <107259694+TrufflePig123@users.noreply.github.com>
2023-05-06 11:39:46 -0400

    Add files via upload
    Finished GameGrid graphics, colors tentative. Created new cell class to better comply with SRP

--- snake/main.py
+++ snake/main.py
@@ -1,11 +1,13 @@
 from controller import Controller
 from model import Model
 from kivy.app import App
+from kivy.graphics import Color, Rectangle, Line
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.screenmanager import Screen , ScreenManager, NoTransition
+from kivy.uix.widget import Widget
 
 class SnakeApp(App):
     '''Kivy base application class. Initalizes the model, view, and controller during application startup. Acts as application entry point.
@@ -42,7 +44,6 @@ class SnakeApp(App):
         gameover_view = GameOverView(name='GameOverView')
 
         self.sm = SnakeScreenManager(title_view, game_view, gameover_view)
-
 
     def build(self):
         '''Built-in kivy method to setup the application root widget. See kivy docs for info: https://kivy.org/doc/stable/api-kivy.app.html   '''
@@ -100,7 +101,53 @@ class GameOverView(Screen):
 
 
 class GameGrid(GridLayout): #Gridlayout? look for other layouts to test and stuff
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.rows = 10
+        self.cols = 10
+
+        for i in range(self.rows*self.cols):
+            self.add_widget(self.create_cell())
+    
+    def create_cell(self): #might be better to export into a different class
+        return GridCell()
+
+
+class GridCell(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        with self.canvas:
+            Color(1, 0, 0) #TODO -- fix color
+            self.rect = Rectangle(pos=self.pos, size=self.size)
+
+        with self.canvas:
+            Color(0, 0, 0) # TODO -- fix color
+            self.border = Line(rectangle=(self.x, self.y, self.width, self.height), width=2)
+        
+        self.bind(pos=self.update_rect, size=self.update_rect)
+        
+    def update_rect(self, instance, value):
+        '''Method to ensure that the drawn rectangle + border reacts to changes in size and position. Code stolen from https://kivy.org/doc/stable/guide/widgets.html#adding-widget-background
+        
+        Parameters
+        ----------
+        instance: Widget
+            The widget that owns the drawn rectangle
+        value: float
+            Represents the rectangle's new size/position values. Required param acessed via Kivy behind-the-scenes magic. 
+            More info here: https://kivy.org/doc/stable/api-kivy.event.html
+        '''
+        
+        instance.rect.pos = instance.pos 
+        instance.rect.size = instance.size
+        #Also update size/pos of the border.
+        instance.border.rectangle = (self.x, self.y, self.width, self.height)
+    
+
+            
+            
+
+
 
 
 #Can't place a direct call to main here. For the class rules defined in kv to reflect on the screens,
